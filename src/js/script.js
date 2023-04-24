26e21473493fbc7653f9ca5991d51439fa52f22d
Andrew Khimich <andrijkhimich@gmail.com>
2023-04-24 18:36:33 +0300

    news section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,55 @@
-//Init Scrollbar
+const toggleMenu = () => {
+  const burger = document.querySelector(".js-burger");
+  const menu = document.querySelector(".js-header-nav");
+  const body = document.querySelector("body");
+  burger.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+      menu.classList.add("active");
+      burger.classList.add("active");
+      body.classList.add("locked");
+    } else {
+      menu.classList.remove("active");
+      burger.classList.remove("active");
+      body.classList.remove("locked");
+    }
+  });
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 992) {
+      menu.classList.remove("active");
+      burger.classList.remove("active");
+      body.classList.remove("locked");
+    }
+  })
+}
+toggleMenu();
 
-// jarallax(document.querySelectorAll('.jarallax'), {
-// 	speed: -0.5,
-// });
+
+const slider = new Swiper(".partners-slider", {
+  grabCursor: true,
+  loop: true,
+  // spaceBetween: 60,
+  // slidesPerView: 7,
+  freeMode: false,
+  allowTouchMove: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // noSwiping: true,
+  speed: 500,
+  // freeModeMomentum: false,
+  // autoplay: {
+  // delay: 0,
+  // disableOnInteraction: true,
+  // },
+  breakpoints: {
+    992: {
+      slidesPerView: 7,
+      spaceBetween: 60
+    },
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+  }
+});
