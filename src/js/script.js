e08e39541881a15432ac8149392bf6271f6aa777
Andrew Khimich <andrijkhimich@gmail.com>
2023-04-25 15:37:08 +0300

    home page ready

--- src/js/script.js
+++ src/js/script.js
@@ -47,9 +47,42 @@ const slider = new Swiper(".partners-slider", {
       slidesPerView: 7,
       spaceBetween: 60
     },
+    767: {
+      slidesPerView: 6,
+      spaceBetween: 30
+    },
+    575: {
+      slidesPerView: 5,
+      spaceBetween: 30
+    },
     320: {
       slidesPerView: 3,
       spaceBetween: 30
     },
   }
 });
+
+
+// load latest video from youtube
+
+// const loadVideo = (iframe) => {
+//   const cid = "UCgJJY_sxYWgwwZYdhI-LCqg";
+//   const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`)
+//   const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
+
+//   fetch(reqURL)
+//     .then(response => response.json())
+//     .then(result => {
+//       console.log(result)
+//       const videoNumber = iframe.getAttribute('videoId')
+//       const link = result.items[videoNumber].link;
+//       const id = link.substr(link.indexOf("=") + 1);
+//       iframe.setAttribute("src", `https://youtube.com/embed/${id}?controls=1&showinfo=0&autohide=1&autoplay=0`);
+//     })
+//     .catch(error => console.log('error', error));
+// }
+
+// const iframes = document.getElementsByClassName('latestVideoEmbed');
+// for (let i = 0, len = iframes.length; i < len; i++) {
+//   loadVideo(iframes[i]);
+// }
