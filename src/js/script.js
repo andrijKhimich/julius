eb917407ff8d40c7dd888d733b0ea9734460b236
Andrew Khimich <andrijkhimich@gmail.com>
2023-04-26 18:46:13 +0300

    desctop

--- src/js/script.js
+++ src/js/script.js
@@ -65,27 +65,27 @@ const slider = new Swiper(".partners-slider", {
 
 // load latest video from youtube
 
-const loadVideo = (iframe) => {
-  const cid = "UCgJJY_sxYWgwwZYdhI-LCqg";
-  const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`)
-  const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
+// const loadVideo = (iframe) => {
+//   const cid = "UCgJJY_sxYWgwwZYdhI-LCqg";
+//   const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`)
+//   const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
 
-  fetch(reqURL)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result)
-      const videoNumber = iframe.getAttribute('videoId')
-      const link = result.items[videoNumber].link;
-      const id = link.substr(link.indexOf("=") + 1);
-      iframe.setAttribute("src", `https://youtube.com/embed/${id}?controls=1&showinfo=0&autohide=1&autoplay=0`);
-    })
-    .catch(error => console.log('error', error));
-}
+//   fetch(reqURL)
+//     .then(response => response.json())
+//     .then(result => {
+//       console.log(result)
+//       const videoNumber = iframe.getAttribute('data-video')
+//       const link = result.items[videoNumber].link;
+//       const id = link.substr(link.indexOf("=") + 1);
+//       iframe.setAttribute("src", `https://youtube.com/embed/${id}?controls=1&showinfo=0&autohide=1&autoplay=0`);
+//     })
+//     .catch(error => console.log('error', error));
+// }
 
-const iframes = document.getElementsByClassName('latestVideoEmbed');
-for (let i = 0, len = iframes.length; i < len; i++) {
-  loadVideo(iframes[i]);
-}
+// const iframes = document.getElementsByClassName('latestVideoEmbed');
+// for (let i = 0, len = iframes.length; i < len; i++) {
+//   loadVideo(iframes[i]);
+// }
 
 // function findVideos() {
 //   let videos = document.querySelectorAll('.video');
